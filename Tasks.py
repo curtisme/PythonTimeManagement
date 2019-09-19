554341a5046bfbe354d3019378dda4dfd52e1b28
Curtis Musgrave-Evans <cmusgrave.evans@gmail.com>
2019-09-19 20:07:52 +1000

    Continued implementation of Task class

--- Tasks.py
+++ Tasks.py
@@ -1,3 +1,6 @@
+from time import time as now
+from time import localtime
+
 class Task:
     def __init__(self, taskDesc, timeListString = "", commentsString = ""):
         def getTimes(timeListString):
@@ -19,6 +22,37 @@ class Task:
                     timeList.append(times)
             return timeList, errors
         self.description = taskDesc
-        self.timeList, self.errors = getTimes(timeListString)
+        #self.timeList, self.errors = getTimes(timeListString)
+        self.timeList, self.errors = [], []
+        self.active = False
+
+    def start(self):
+        if not self.active:
+            self.timeList.append([int(now()), -1])
+            self.active = True
+
+
+    def stop(self):
+        if self.active:
+            self.timeList[-1][1] = int(now())
+            self.active = False
+
+    def __str__(self):
+        out = "{}:\n".format(self.description)
+        for time in self.timeList:
+            start = localtime(time[0])
+            if time[1] != -1:
+                stop = localtime(time[1])
+                endString = "{}:{}.{}".format(stop.tm_hour, stop.tm_min, stop.tm_sec)
+            else:
+                endString = "?"
+            out += "{}:{}.{} - {}\n".format(start.tm_hour, start.tm_min, start.tm_sec, endString)
+        return out
+    
+    def toCSVRow(self):
+        timeList = ""
+        for time in self.timeList:
+            timeList += "{}:{}|".format(time[0], time[1])
+        return "{},{}".format(self.description, timeList)
 
 
